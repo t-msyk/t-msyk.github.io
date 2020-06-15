72fca1617e66f9ca58aabe064216c2fa441e16d7
t-msyk <tam.msyk@gmail.com>
2020-06-16 00:55:09 +0900

    Remove debug message

--- sfenview/sfenview.js
+++ sfenview/sfenview.js
@@ -26,7 +26,7 @@ function go_to_end () {
 function draw () {
   var selected = document.getElementById("form_list").value;
   draw_sfen(sfen_list[selected]);
-  console.log(selected);
+  //console.log(selected);
 }
 function draw_board ( board ) {
   var extended_board = board.replace(/9/g,"_________")
@@ -86,7 +86,7 @@ function draw_board ( board ) {
                           .replace(/P/,"歩")
     ++c;
   }
-  console.log(extended_board);
+  //console.log(extended_board);
 }
 function draw_hand ( hand ) {
   var whand=hand.replace(/(\d*K)*(\d*R)*(\d*B)*(\d*G)*(\d*S)*(\d*N)*(\d*L)*(\d*P)*/g,"")
@@ -116,10 +116,10 @@ function draw_sfen ( sfen ) {
   var turn  = tmp[1];
   var hand  = tmp[2];
   var tempo = tmp[3];
-  console.log("board:"+board);
+  //console.log("board:"+board);
   draw_board(board);
   document.getElementById("turn").innerHTML = (turn==="b")?"先手番":"後手番";
-  console.log("hand :"+hand);
+  //console.log("hand :"+hand);
   draw_hand(hand);
   document.getElementById("tempo").innerHTML = tempo + "手目";
 }
